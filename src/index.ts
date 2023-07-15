4971b281a3e67a232a63bbe0b5745037005c6a07
Parvind <36909520+MapleSage@users.noreply.github.com>
2023-07-15 11:34:17 +0530

    Updated the .env.example file to include the new environment variables.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable dot-notation */
 import * as dotenv from "dotenv";
 import { Vector, utils } from '@pinecone-database/pinecone';
 import { getEnv } from "utils/util.ts";
@@ -15,56 +13,59 @@ const { createIndexIfNotExists, chunkedUpsert } = utils;
 
 const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
-// Index setup
-const indexName = getEnv("PINECONE_INDEX");
-const pineconeClient = await getPineconeClient();
+async function main() {
+  // Index setup
+  const indexName = getEnv("PINECONE_INDEX");
+  const pineconeClient = await getPineconeClient();
 
+  async function getChunk(df: dfd.DataFrame, start: number, size: number): Promise<dfd.DataFrame> {
+    return await df.head(start + size).tail(size);
+  }
 
-async function getChunk(df: dfd.DataFrame, start: number, size: number): Promise<dfd.DataFrame> {
-  // eslint-disable-next-line no-return-await
-  return await df.head(start + size).tail(size);
-}
+  async function* processInChunks(dataFrame: dfd.DataFrame, chunkSize: number): AsyncGenerator<Document[]> {
+    for (let i = 0; i < dataFrame.shape[0]; i += chunkSize) {
+      const chunk = await getChunk(dataFrame, i, chunkSize);
+      const records = dfd.toJSON(chunk) as SquadRecord[];
+      yield records.map((record: SquadRecord) => new Document({
+        pageContent: record.context,
+        metadata: {
+          id: record["id"],
+          question: record["question"],
+          answer: record["answer"],
+          context: record["context"],
+        },
+      }));
+    }
+  }
 
-async function* processInChunks(dataFrame: dfd.DataFrame, chunkSize: number): AsyncGenerator<Document[]> {
-  for (let i = 0; i < dataFrame.shape[0]; i += chunkSize) {
-    const chunk = await getChunk(dataFrame, i, chunkSize);
-    const records = dfd.toJSON(chunk) as SquadRecord[];
-    yield records.map((record: SquadRecord) => new Document({
-      pageContent: record.context,
-      metadata: {
-        id: record["id"],
-        question: record["question"],
-        answer: record["answer"],
-        context: record["context"],
-      },
-    }));
+  async function embedAndUpsert(dataFrame: dfd.DataFrame, chunkSize: number) {
+    const chunkGenerator = processInChunks(dataFrame, chunkSize);
+    const index = pineconeClient.Index(indexName);
+
+    for await (const documents of chunkGenerator) {
+      await embedder.embedBatch(documents, chunkSize, async (embeddings: Vector[]) => {
+        await chunkedUpsert(index, embeddings, "default");
+        progressBar.increment(embeddings.length);
+      });
+    }
+  }
+
+  try {
+    const squadData = await loadSquad();
+    await createIndexIfNotExists(pineconeClient, indexName, 384);
+    progressBar.start(squadData.shape[0], 0);
+    await embedder.init("Xenova/all-MiniLM-L6-v2");
+    await embedAndUpsert(squadData, 1);
+
+    progressBar.stop();
+    console.log(`Inserted ${progressBar.getTotal()} documents into index ${indexName}`);
+
+  } catch (error) {
+    console.error(error);
   }
 }
 
-async function embedAndUpsert(dataFrame: dfd.DataFrame, chunkSize: number) {
-  const chunkGenerator = processInChunks(dataFrame, chunkSize);
-  const index = pineconeClient.Index(indexName);
-
-
-  for await (const documents of chunkGenerator) {
-    await embedder.embedBatch(documents, chunkSize, async (embeddings: Vector[]) => {
-      await chunkedUpsert(index, embeddings, "default");
-      progressBar.increment(embeddings.length);
-    });
-  }
-}
-
-try {
-  const squadData = await loadSquad();
-  // squadData.print();
-  await createIndexIfNotExists(pineconeClient, indexName, 384);
-  progressBar.start(squadData.shape[0], 0);
-  await embedder.init("Xenova/all-MiniLM-L6-v2");
-  await embedAndUpsert(squadData, 1);
-
-  progressBar.stop();
-  console.log(`Inserted ${progressBar.getTotal()} documents into index ${indexName}`);
-
-} catch (error) {
+// Call the main function.
+main().catch((error) => {
   console.error(error);
-}
+});
